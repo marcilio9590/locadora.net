1399bb7855290022285514bf1bf0d89edd9eebd8
marcilio9590 <marcilio9590@gmail.com>
2017-11-15 01:55:47 -0200

    criação dos dtos.

---
+++ Locadora-Filmes/frmTelaHome.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace Locadora_Filmes
+{
+    partial class frmTelaHome
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // frmTelaHome
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(712, 450);
+            this.Name = "frmTelaHome";
+            this.Text = "Home";
+            this.Load += new System.EventHandler(this.frmTelaHome_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ Locadora.DTO/PessoaDTO.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Locadora.DTO
+{
+    class PessoaDTO
+    {
+        private int codigo;
+        private String nome;
+        private String rua;
+        private String numeroCasa;
+        private String cep;
+        private String bairro;
+        private String cidade;
+        private String cpf;
+        private String rg;
+        private String sexo;
+        private DateTime dataNascimento;
+        private String telefone;
+
+        public int Codigo { get => codigo; set => codigo = value; }
+        public string Nome { get => nome; set => nome = value; }
+        public string Rua { get => rua; set => rua = value; }
+        public string NumeroCasa { get => numeroCasa; set => numeroCasa = value; }
+        public string Cep { get => cep; set => cep = value; }
+        public string Bairro { get => bairro; set => bairro = value; }
+        public string Cidade { get => cidade; set => cidade = value; }
+        public string Cpf { get => cpf; set => cpf = value; }
+        public string Rg { get => rg; set => rg = value; }
+        public string Sexo { get => sexo; set => sexo = value; }
+        public DateTime DataNascimento { get => dataNascimento; set => dataNascimento = value; }
+        public string Telefone { get => telefone; set => telefone = value; }
+    }
+}
